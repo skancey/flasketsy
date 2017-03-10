48de3c8d5fe03e50d84a6dfcf8bcf745a12744e1
Siddiq Kancey <skancey@24dcaea7d349>
2017-03-10 20:10:52 +0000

    Add Delete Listing Page

--- flask_app.py
+++ flask_app.py
@@ -73,6 +73,12 @@ def listing_edit(listing_id):
             abort(404)
         return render_template('listing_edit.html', form=lstForm)
 
+@app.route('/listing/delete/<listing_id>', methods=['GET','POST'])
+def listing_delete(listing_id):
+    Listing.query.filter_by(id=listing_id).delete()
+    db.session.commit()
+    return render_template('listing_delete_confirm.html', listingid=listing_id)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
